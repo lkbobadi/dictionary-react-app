7d0606edcf1a02571cd0bbfeaeb608350ec8afdb
lkbobadi <lkbobadi@gmail.com>
2022-03-04 16:59:36 +0900

    Mobile Responsiveness added

--- src/Results.js
+++ src/Results.js
@@ -5,8 +5,8 @@ import Phonetics from "./Phonetics";
 export default function Results(props) {
   if (props.results) {
     return (
-      <div className='results'>
-        <div className='content-section mt-5 pe-0'>
+      <div className='results p-0'>
+        <div className='content-section g-0 mt-2'>
           <h3>{props.results.word}</h3>
           <Phonetics phonetics={props.results.phonetics} />
           {props.results.meanings.map(function (meaning, index) {
