9deca03b28e69ad0af2f14e3f3316f4a7b96724e
lkbobadi <lkbobadi@gmail.com>
2022-03-04 19:38:33 +0900

    alt text src changed

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,11 @@ export default function Photos(props) {
             <div className='col-4' key={index}>
               <div className='m-2 '>
                 <a href={photo.src.original} target='_blank' rel='noreferrer'>
-                  <img src={photo.src.landscape} className='img-fluid' alt='' />
+                  <img
+                    src={photo.src.landscape}
+                    className='img-fluid'
+                    alt={photo.src.photographer}
+                  />
                 </a>
               </div>
             </div>
