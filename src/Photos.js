e9e5b55406eb600720c1dde3b7a7e8141f8a850d
lkbobadi <lkbobadi@gmail.com>
2022-03-04 19:29:36 +0900

    alt tag added

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,7 @@ export default function Photos(props) {
             <div className='col-4' key={index}>
               <div className='m-2 '>
                 <a href={photo.src.original} target='_blank' rel='noreferrer'>
-                  <img src={photo.src.landscape} className='img-fluid' />
+                  <img src={photo.src.landscape} className='img-fluid' alt='' />
                 </a>
               </div>
             </div>
